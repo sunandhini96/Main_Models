ece179002dad7897a44ae9ecfedcdbbb32c812c8
sunandhini96 <63030539+sunandhini96@users.noreply.github.com>
2023-02-18 02:00:21 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -132,3 +132,91 @@ def misclassification():
       ax = fig.add_subplot(6, 10, idx+1, xticks=[], yticks=[])
       plt.imshow(im_convert(images[idx]))#gt ground truth
       ax.set_title(" pred {} gt ({})".format(str(classes[preds[idx].item()]), str(classes[labels[idx].item()])), color=("green" if preds[idx]==labels[idx] else "red"))
+    
+def viz_data(cols=8, rows=5):
+  figure = plt.figure(figsize=(14, 10))
+  for i in range(1, cols * rows + 1):
+    img, label = exp[i]
+
+    figure.add_subplot(rows, cols, i)
+    plt.title(exp.classes[label])
+    plt.axis("off")
+    plt.imshow(img, cmap="gray")
+
+  plt.tight_layout()
+  plt.show()
+    
+  def show_images(aug_dict, ncol=6):
+  nrow = len(aug_dict)
+
+  fig, axes = plt.subplots(ncol, nrow, figsize=( 3*nrow, 15), squeeze=False)
+  for i, (key, aug) in enumerate(aug_dict.items()):
+    for j in range(ncol):
+      ax = axes[j,i]
+      if j == 0:
+        ax.text(0.5, 0.5, key, horizontalalignment='center', verticalalignment='center', fontsize=15)
+        ax.get_xaxis().set_visible(False)
+        ax.get_yaxis().set_visible(False)
+        ax.axis('off')
+      else:
+        image, label = exp[j-1]
+        if aug is not None:
+          transform = A.Compose([aug])
+          image = np.array(image)
+          image = transform(image=image)['image']
+          
+        ax.imshow(image)
+        ax.set_title(f'{exp.classes[label]}')
+        ax.axis('off')
+
+  plt.tight_layout()
+  plt.show()
+  
+# custom dataset class for albumentations library
+class AlbumentationImageDataset(Dataset):
+  def __init__(self, image_list, train= True):
+      self.image_list = image_list
+      self.aug = A.Compose({
+          A.PadIfNeeded(min_height=32+4,min_width=32+4),
+          A.RandomCrop(32,32,always_apply=False,p=1.0),
+          A.Normalize((0.49139968, 0.48215841, 0.44653091), (0.24703223, 0.24348513, 0.26158784)),
+          A.HorizontalFlip(),
+          A.Cutout(1,8,8,fill_value=0.473363),
+          # A.CoarseDropout(1, 16, 16, 1, 16, 16,fill_value=0.473363, mask_fill_value=None),
+          A.ToGray()
+      })
+
+      self.norm = A.Compose({A.Normalize((0.49139968, 0.48215841, 0.44653091), (0.24703223, 0.24348513, 0.26158784)),
+      })
+      self.train = train
+        
+  def __len__(self):
+      return (len(self.image_list))
+
+  def __getitem__(self, i):
+      
+      image, label = self.image_list[i]
+      
+      if self.train:
+        #apply augmentation only for training
+        image = self.aug(image=np.array(image))['image']
+      else:
+        image = self.norm(image=np.array(image))['image']
+      image = np.transpose(image, (2, 0, 1)).astype(np.float32)
+      return torch.tensor(image, dtype=torch.float), label
+ from torch_lr_finder import LRFinder
+
+
+def find_lr(net, optimizer, criterion, train_loader):
+    """Find learning rate for using One Cyclic LRFinder
+    Args:
+        net (instace): torch instace of defined model
+        optimizer (instance): optimizer to be used
+        criterion (instance): criterion to be used for calculating loss
+        train_loader (instance): torch dataloader instace for trainig set
+    """
+    lr_finder = LRFinder(net, optimizer, criterion, device="cuda")
+    lr_finder.range_test(train_loader, end_lr=10, num_iter=100, step_mode="exp")
+    lr_finder.plot()
+    lr_finder.reset()
+  
