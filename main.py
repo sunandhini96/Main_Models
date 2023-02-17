21304ab1b5f5a2bc17a4537ddd79acc3a0b33390
sunandhini96 <63030539+sunandhini96@users.noreply.github.com>
2023-02-18 02:06:30 +0530

    Update main.py

--- main.py
+++ main.py
@@ -54,17 +54,7 @@ from Models import resnet
 
 from Models.resnet import *
 # loading the dataset
-exp = datasets.CIFAR10('./data', train=True, download=True)
-exp_data = exp.data
 
-# Calculate the mean and std for normalization
-print('[Train]')
-print(' - Numpy Shape:', exp_data.shape)
-print(' - min:', np.min(exp_data, axis=(0,1,2)) / 255.)
-print(' - max:', np.max(exp_data, axis=(0,1,2)) / 255.)
-print(' - mean:', np.mean(exp_data, axis=(0,1,2)) / 255.)
-print(' - std:', np.std(exp_data, axis=(0,1,2)) / 255.)
-print(' - var:', np.var(exp_data, axis=(0,1,2)) / 255.)
 
 use_cuda = torch.cuda.is_available()
 device = torch.device("cuda" if use_cuda else "cpu")
